97ca3f0fc891cf7008d010255e669c4932f5d813
marques-j-robinson <marques.j.robinson@gmail.com>
2024-04-06 03:38:07 -0700

    organize modules and create a abstract layer for creating dom elements

--- src/index.js
+++ src/index.js
@@ -7,10 +7,11 @@
 // About
 // Photo by <a href="https://unsplash.com/@shootdelicious?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Eiliv Aceron</a> on <a href="https://unsplash.com/photos/a-man-cooking-food-in-a-kitchen-wNQoaYCFcsI?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
 
-import './style.css'
-import {createContent, createImg} from './home.js'
+import './assets/style.css'
+import {createContent, createImg} from './lib/page.js'
+import {pageInfo as homePageInfo} from './lib/home.js'
 
 const rootEl = document.querySelector('#root');
 
-rootEl.append(createContent())
-rootEl.append(createImg())
+rootEl.append(createContent(homePageInfo))
+rootEl.append(createImg(homePageInfo))
