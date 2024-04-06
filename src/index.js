14e83f61a817dd6eddc567bb72a92d3c42c6bdcd
marques-j-robinson <marques.j.robinson@gmail.com>
2024-04-06 02:59:19 -0700

    create home page mockup with html, styles and images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hello, World!')
+// Home
+// Photo by <a href="https://unsplash.com/@ninjason?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Jason Leung</a> on <a href="https://unsplash.com/photos/photo-of-pub-set-in-room-during-daytime-poI7DelFiVA?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+//
+// Menu
+// Photo by <a href="https://unsplash.com/@jonathanborba?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Jonathan Borba</a> on <a href="https://unsplash.com/photos/person-slicing-vegetable-uB7q7aipU2o?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+//
+// About
+// Photo by <a href="https://unsplash.com/@shootdelicious?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Eiliv Aceron</a> on <a href="https://unsplash.com/photos/a-man-cooking-food-in-a-kitchen-wNQoaYCFcsI?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+
+import HomeImg from './home.jpg'
+
+const contentEl = document.querySelector('#content');
+
+const homeImg = new Image();
+homeImg.src = HomeImg;
+
+contentEl.append(homeImg)
